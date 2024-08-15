33a4ac58e17d7b62b8cabce9a0220c3ba29012b2
Duc-Phu Ng. <sillycat@duck.com>
2024-08-15 12:06:41 +0700

    chore(setup): configure vim options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,6 +1,4 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+# MewEluk | Configuring Vim Options
 vim.g.mapleader = " "
 
 vim.scriptencoding = "utf-8"
